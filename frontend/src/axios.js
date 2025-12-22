1d878540164cd63bad064fa0a9ab45d2e38e6f82
PABBU SAKETH <147064850+saketh169@users.noreply.github.com>
2025-12-22 06:19:10 +0000

    update

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 // Set backend API URL
-axios.defaults.baseURL = 'https://nutri-connect-backend.vercel.app/';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 export default axios;
